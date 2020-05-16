4a22dcc9983d2618624d2649518d3bedbdb7d9cc
spyentist <spyentist@yahoo.com>
2020-05-16 14:54:09 -0600

    Week 4 updates

---
+++ week_3/scripts/script.js
@@ -0,0 +1,56 @@
+function alertMe() {
+    alert("The code makes it to the reference!")
+}
+
+
+function doInputOutput() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    var hh = today.getHours();
+    var mn = today.getMinutes();
+    var ss = today.getSeconds();
+    
+    if (hh > 12) {
+        hh = hh - 12
+    }
+    
+    if (hh < 10) {
+        hh = "0"+hh
+    }
+    
+    if (dd < 10) {
+        dd = "0"+dd
+    }
+    
+    if (mm < 10) {
+        mm = "0"+mm
+    }
+    
+    if (mn < 10) {
+        mn = "0"+mn
+    }
+    
+    if (ss < 10) {
+        ss = "0"+ss
+    }
+    
+    
+    
+    
+    
+    var display = "Last Updated: " + mm + "/" + dd + "/" + yyyy + "   " + hh + ":" + mn + ":" + ss;
+    var display2 = "Last Updated: " + document.lastModified;
+
+    document.getElementById("updateDate").innerHTML = display2;
+    document.getElementById("curYear").innerHTML = yyyy;
+    // alert("This works!")
+}
+
+
+function toggleMenu () {
+    document.querySelector('#navigation').classList.toggle("open");
+}
+
+document.getElementById("menu").addEventListener('click', toggleMenu);
