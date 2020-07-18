4863973abd08370fdf1dbf99d585f18aa0cf145b
spyentist <spyentist@yahoo.com>
2020-07-18 02:26:20 -0600

    JS changes

--- Final_project/scripts/main.js
+++ Final_project/scripts/main.js
@@ -14,52 +14,112 @@ function website (url, id) {
     .then( response => response.json())
     .then(jsObject => {
 
-        // console.table(jsObject);
-        console.log(jsObject.url);
-        let switchTest = jsObject.url;
-        console.log(switchTest)
-
-        // switch (switchTest){
-        //   case switchTest.includes('vehicles'):
-        //     alert('vehicles');
-        //     break;
-          
-        // }
-
-        view(jsObject.results, id, jsObject);
+        console.table(jsObject.next);
+        console.log(`url = ${id}`);
+		console.log(`test ${url.includes(`vehicles/\d/`)}`);
+				
+		if (id === 'data') {
+			console.log('DATA');
+			if (url.includes(`vehicles`)) {
+				console.log('vehicles');	
+				viewIndiv(jsObject, 'vehicles')			
+			} else if (url.includes(`films`)) {
+				console.log('films');
+				viewIndiv(jsObject, 'films')
+			} else if (url.includes(`people`)) {
+				console.log('people');
+				viewIndiv(jsObject, 'people')
+			} else if (url.includes(`starships`)) {
+				console.log('starships');
+				viewIndiv(jsObject, 'starships')
+			} else if (url.includes(`species`)) {
+				console.log('species');
+				viewIndiv(jsObject, 'species')
+			}
+	 	// } else if (id === 'recursive') { 
+		// 	console.log('Next page');
+		// 	return jsObject.results
+		} else {
+			console.log('ELSE');
+			console.log(url);
+			viewPage(jsObject.results, id, jsObject.next);
+		}
     })
     .catch( error => console.log('There was an error!', error) );
 }
 
+function viewIndiv(obj, id) {
+	let div = document.getElementById('swapi');
+	div.innerHTML = '';
+	let list = '';
+	let span = document.createElement('span');
+	let ul = document.createElement('ul');
 
-// Functional for the main view list
-function view(obj, id, r2d2) {
-  let div = document.getElementById('swapi');
-  div.innerHTML = '';
-  let span = document.createElement('span');
-  let ul = document.createElement('ul');
-  let list = '';
-  let count = 0;
-
-  div.appendChild(span);
-  span.appendChild(ul);
-
-  // console.log('id', id);
-  list += `<li><h4>${id.toUpperCase()}</h4></li>`;
-  obj.forEach((x) => {
-    let chewy = (id =='films') ? x.title : x.name;
-    console.log('chewy', chewy, id, x.url);
-    
-    list += `
-      <li id='${count}' class='data' data-site='${x.url}'>${chewy}</li>
-    `;
-    count++;
-  });
-
-  ul.innerHTML = list;
-  paginations(r2d2);
-  refreshData();
+	let count = 0;
+  
+	div.appendChild(span);
+	span.appendChild(ul);
+  
+	// console.log('id', id);
+	list += `<li><h4>${id.toUpperCase()}</h4></li>`;
+	// name, model, vehicle_class, manufacturer, length, cost_in_credits, crew, passengers, max_atmoshpere_speed, cargo_capacity
+	Object.keys(obj).forEach((key/*:string*/) => {
+		if (obj[key] != undefined) {
+			if (key != 'created' && key != 'edited' & key != 'url') {
+				let value = obj[key]
+				list += `<li>${key.toLocaleUpperCase()}: </li><li>${value.toString().replace('_', ' ')}</li><br>`
+			} else {
+				console.log(key,obj[key])
+			}
+		}
+	})
+	ul.innerHTML = list;
+	// paginations()
 }
+
+let pagecount = 1
+let list = '';
+let count = 0;
+let div = document.getElementById('swapi');
+let span = document.createElement('span');
+let ul = document.createElement('ul');
+div.innerHTML = '';
+
+
+
+// Functional for the main viewPage list
+function viewPage(obj, id, r2d2) {
+
+	div.appendChild(span);
+	span.appendChild(ul);
+	if (r2d2 != undefined) {
+		// console.log('id', id);
+		list += `<li><h4>${id.toUpperCase()}</h4></li>`;
+
+		obj.forEach((x) => {
+			let chewy = (id =='films') ? x.title : x.name;
+			console.log('chewy', chewy, id, x.url);
+			let https = insertSecure(x.url)
+			
+			list += `
+			<li id='${count}' class='data' data-site='${https}'>${chewy}</li>
+			`;
+			count++;
+		});
+		console.log(r2d2.next);
+		obj,r2d2 = website(r2d2, id)
+	}
+
+		ul.innerHTML = list;
+		// paginations(r2d2);
+		refreshData();
+}
+// DONE
+function insertSecure(url) {
+  return url.slice(0, 4) + 's' + url.slice(4 + Math.abs(0));
+}
+
+
 
 
 // NEED TO FINISH
@@ -90,16 +150,17 @@ function paginations(r2d2)  {
 //setting and calling adaptive menu for app
 
 document.querySelector(".menu_icon").addEventListener("touchend", adaptiveMenu);
-
+// DONE
 function adaptiveMenu() {
     let links = document.getElementById('navigation')
     if(links.className === "navigation") {
         links.className +=".responsive"
     } else {
         links.className ="navigation"
-    }
+    } 
 }
 
+// Setting the temp url for the main page view
 document.querySelectorAll('.links').forEach(item => {
     item.addEventListener('touchend', event => { 
         let temp_url = url + item.id + "/";
@@ -110,14 +171,14 @@ document.querySelectorAll('.links').forEach(item => {
     )}); 
 
 
+// Adding the data-site tag to the data and adding the event listener
 function refreshData(){
 document.querySelectorAll('.data').forEach(item => {
   item.addEventListener('touchend', event => { 
-      let temp_url = '';
       console.log(item.getAttribute('data-site'))
-      temp_url = item.getAttribute('data-site')
-      console.log(item);
-      website(temp_url, 'test');
+	  let temp_url = item.getAttribute('data-site')
+    //   console.log(item);
+      website(temp_url, 'data');
   }
   )}); 
 
